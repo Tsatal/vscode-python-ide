6ff04912a3590950f96de1e77c6e56eaac5d5d90
Tsatal <nikolastsatal@gmail.com>
2026-01-12 21:28:21 +0200

    Initial commit

---
+++ student.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+
+    def status(self):
+        return f"Ο/Η {self.name} έχει βαθμό {self.grade}"
+
+# Δημιουργία αντικειμένου
+s1 = Student("Γιώργος", 18)
+print(s1.status())
